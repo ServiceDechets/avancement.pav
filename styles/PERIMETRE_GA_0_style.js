07e21eaf64581dcb77f492c8cfec25506f613175
ServiceDechets <cveyssiere.ga@gmail.com>
2024-06-27 16:59:06 +0300

    Add files via upload

--- styles/PERIMETRE_GA_0_style.js
+++ styles/PERIMETRE_GA_0_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_COMMUNE_GA_1 = function(feature, resolution){
+var style_PERIMETRE_GA_0 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -9,19 +9,19 @@ var style_COMMUNE_GA_1 = function(feature, resolution){
     var value = ""
     var labelText = "";
     size = 0;
-    var labelFont = "10.4px \'Arial\', sans-serif";
-    var labelFill = "#323232";
-    var bufferColor = "#fafafa";
-    var bufferWidth = 0.26;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
     var textAlign = "left";
     var offsetX = 8;
     var offsetY = 3;
     var placement = 'point';
-    if (feature.get("NOM") !== null) {
-        labelText = String(feature.get("NOM"));
+    if ("" !== null) {
+        labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(141,90,153,0.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.976}),fill: new ol.style.Fill({color: 'rgba(232,113,141,0.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
